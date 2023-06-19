368f4d4c19ebf5bbeef696afe4bcb008c0c8ddd2
TurovaE <ekaterina.turova@mail.ru>
2023-06-20 00:58:24 +0700

    add structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [cards, setCards] = useState([
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis veritatis suscipit ab impedit magnam voluptates, eligendi aliquam dolores vitae! Distinctio architecto minima, voluptate doloribus eveniet dolores magni neque tenetur excepturi?",
+  ]);
+
   return (
     <div className="app">
-      <h1>App</h1>
+      <div className="notes">
+        <div className="header">
+          <h2 className="header-title">Notes</h2>
+          <button type="button" className="btn btn-update" />
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="card-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Perferendis veritatis suscipit ab impedit magnam voluptates,
+              eligendi aliquam dolores vitae! Distinctio architecto minima,
+              voluptate doloribus eveniet dolores magni neque tenetur excepturi?
+            </div>
+            <button type="button" className="btn btn-delete" />
+          </div>
+          <form action="" className="add-note">
+            <h3>New Note</h3>
+            <textarea name="" id="" cols={40} rows={10}></textarea>
+            <button type="submit" className="btn btn-send" />
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
